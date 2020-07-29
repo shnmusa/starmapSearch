c82cb21e54234ddddb1109bca2adc88e555a1565
Musa Servan Sahin [Global Yazilim Gelistirme Servisi] <MusaServan.Sahin@ing.com.tr>
2020-07-29 16:47:51 +0300

    Initial Commit

---
+++ CVProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CVProject
+//
+//  Created by Musa Servan Sahin [Global Yazilim Gelistirme Servisi] on 29.07.2020.
+//  Copyright © 2020 Musa Servan Sahin [Global Yazilim Gelistirme Servisi]. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
